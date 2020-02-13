3e06328f43b2357f183540e39e4019609d4dfeb4
Antoine Desaché <antoine.desache@gmail.com>
2020-02-13 15:10:09 +0100

    implemented back-end

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+require 'faker'
+
+puts 'Clean DB'
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+puts 'Ingredients DB cleaned'
+puts '----------------------'
+puts 'Creating cocktail ingredients'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create!(name: ingredient["strIngredient1"])
+end
+
+10.times do
+  cocktail = Cocktail.new(
+    name: Faker::Beer.name
+  )
+  cocktail.save!
+  3.times do
+    dose = Dose.new(
+      description: Faker::Number.between(from: 1, to: 10),
+      ingredient: Ingredient.all.sample,
+      cocktail_id: cocktail.id
+    )
+    dose.save!
+  end
+  puts "created #{cocktail.name} -- #{}"
+end
+puts 'Finished'
+puts "#{Ingredient.count} ingredients created"
